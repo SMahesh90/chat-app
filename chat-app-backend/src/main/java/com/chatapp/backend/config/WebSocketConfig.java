4bca55268ca1b0021ecf276938b3a5c5cbf8418a
Sampatharao Mahesh Kumar <sampatharaomahesh@gmail.com>
2026-09-03 18:02:06 +0530

    After pasting the forntend url in CorsConfig and WebSocketController

--- chat-app-backend/src/main/java/com/chatapp/backend/config/WebSocketConfig.java
+++ chat-app-backend/src/main/java/com/chatapp/backend/config/WebSocketConfig.java
@@ -1,13 +1,14 @@
 package com.chatapp.backend.config;
 
-import com.chatapp.backend.security.JwtHandshakeInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
 import com.chatapp.backend.security.CustomHandshakeHandler;
+import com.chatapp.backend.security.JwtHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -26,7 +27,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("http://localhost:5173")
+                .setAllowedOriginPatterns("luminous-flow-production-d83c.up.railway.app")
                 .addInterceptors(jwtHandshakeInterceptor)
                 .setHandshakeHandler(new CustomHandshakeHandler())
                 .withSockJS();
